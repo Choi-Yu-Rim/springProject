bce6b17fbcdb1ff0ec5f116fb5e17155796d0fd5
choiyurum <iam_ip@naver.com>
2023-01-17 23:55:58 +0900

    Initial commit

---
+++ src/test/java/com/web/HelloControllerTest.java
@@ -0,0 +1,25 @@
+package com.web;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import www.SpringBoot.com.web.HelloController;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() {
+        String hello = "hello";
+        mvc.perform(get("/Hello")).andExpect(stat)
+
+    }
+}
